085d1e4419bf11036fa39fff0f50e1fd1654eb97
BitFloyd <sby.jcub@gmail.com>
2020-06-02 22:38:05 -0400

    change home env

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-
-IMAGE_METADATA_FOLDER = '/Users/sebyjacob/My_Projects/OpenSourceGAN/OpenImagesV6/image_metadata'
+HOME = os.getenv(PROJECT_BASE_PATH,'/Users/sebyjacob/My_Projects/OpenSourceGAN')
+IMAGE_METADATA_FOLDER = os.path.join(HOME,'OpenImagesV6/image_metadata')
 IMAGE_CLASS_DESCRIPTIONS_FILE = os.path.join(IMAGE_METADATA_FOLDER, 'oidv6-class-descriptions.csv')
 TRAIN_BBOX_ANNOTATIONS_FILE = os.path.join(IMAGE_METADATA_FOLDER, 'oidv6-train-annotations-bbox.csv')
 TEST_BBOX_ANNOTATIONS_FILE = os.path.join(IMAGE_METADATA_FOLDER, 'test-annotations-bbox.csv')
@@ -8,7 +8,7 @@ VAL_BBOX_ANNOTATIONS_FILE = os.path.join(IMAGE_METADATA_FOLDER, 'validation-anno
 IMAGES_DICT_FOR_LABEL_META = os.path.join(IMAGE_METADATA_FOLDER,'images-for-label-meta')
 IMAGE_IDS_FOLDER=os.path.join(IMAGE_METADATA_FOLDER,'image-ids')
 LIST_IMAGE_URL_FILES = [os.path.join(IMAGE_IDS_FOLDER, i) for i in os.listdir(IMAGE_IDS_FOLDER) if 'csv' in i]
-LABEL_HIERARCHY_JSON='/Users/sebyjacob/My_Projects/OpenSourceGAN/OpenImagesV6/bbox_labels_600_hierarchy.json'
+LABEL_HIERARCHY_JSON=os.path.join(HOME,'OpenImagesV6/bbox_labels_600_hierarchy.json')
 
-IMAGES_ROOT = '/Users/sebyjacob/My_Projects/OpenSourceGAN/OpenImagesV6/images'
+IMAGES_ROOT = os.path.join(HOME,'OpenImagesV6/images')
 
