0378778366faa01252c870c40cb79dc2af33cad1
BitFloyd <sby.jcub@gmail.com>
2020-06-06 22:12:00 -0400

    change size, add leaky relu

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ IMAGE_IDS_FOLDER = os.path.join(IMAGE_METADATA_FOLDER, 'image-ids')
 LIST_IMAGE_URL_FILES = [os.path.join(IMAGE_IDS_FOLDER, i) for i in os.listdir(IMAGE_IDS_FOLDER) if 'csv' in i]
 LABEL_HIERARCHY_JSON = os.path.join(HOME, 'OpenImagesV6/bbox_labels_600_hierarchy.json')
 
-IMAGES_ROOT = os.path.join(DATA_HOME, '/OpenImages/images')
+IMAGES_ROOT = os.path.join(DATA_HOME, 'OpenImages/images')
 
 DISCRIMINATOR_BASEMODEL = 'INCEPTION_V3'
 
